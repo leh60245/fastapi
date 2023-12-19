5332d60993693fa531d60285496d3220c8f15ae6
LeeEuiHyoung <kkh60245@gmail.com>
2023-12-19 20:05:14 +0900

    필요없는 파일들 삭제

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
-from fastapi import FastAPI, File, UploadFile, HTTPException
+from fastapi import FastAPI, HTTPException
 from io import BytesIO
 from PIL import Image
 import base64
 import numpy as np
 from pydantic import BaseModel
 from tensorflow.keras.models import load_model
-import imageio
-
 from starlette.middleware.cors import CORSMiddleware
-from fastapi.responses import StreamingResponse
 
 # from domain.question import question_router
 
@@ -36,16 +33,18 @@ model = load_model("i_model.h5", compile=False)
 @app.post("/uploadfile/")
 async def create_upload_file(image_data: ImageData):
     try:
+        # 원본 이미지를 image에 저장합니다.
         header, encoded = image_data.image_str.split(",", 1)  
-        # print(encoded)
         image_bytes = base64.b64decode(encoded)
         image = Image.open(BytesIO(image_bytes)).convert('RGB')
 
-        # 이미지 처리 로직
+        # 모델에 맞는 인풋 형태로 변형합니다.
         image = image.resize((64, 64))
         image_array = np.array(image) / 255.0
         image_array = np.expand_dims(image_array, axis=0)
 
+        # 이미지를 예측하고 결과를 반환합니다.
+        # 반환하는 결과는 1. 예측한 클래스: int, 2. 각 클래스별 예측한 값: List<int>
         result = model.predict(image_array)
         result_list = result.tolist()[0]
         class_0_prob = result_list[0]
